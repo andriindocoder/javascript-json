9fcd95cdd6c193a058039e8ad5934e8d60c13b59
Unknown <andriwicaksonost@gmail.com>
2018-07-14 14:22:22 +0700

    JSONP for Access-Control-Allow-Origin

--- myscript.js
+++ myscript.js
@@ -1,29 +1,17 @@
-var request;
-if (window.XMLHttpRequest) {
-	request = new XMLHttpRequest();
-} else {
-	request = new ActiveXObject("Microsoft.XMLHTTP");
-}
+function dataHandler(info){
 
-request.open('GET', 'data.json');
-request.onreadystatechange = function() {
-	if((request.status == 200) && (request.readyState === 4)){
-		var info = JSON.parse(request.responseText);
-
-		var output = '';
-		for (var i =0; i<=info.links.length-1; i++) {
-			for (key in info.links[i]) {
-				if (info.links[i].hasOwnProperty(key)) {
-					output += '<li>' +
-					'<a href= "' + info.links[i][key] +
-					'">' + key + '</a>';
-					'</li>';
-				}
+	var output = '';
+	for (var i =0; i<=info.links.length-1; i++) {
+		for (key in info.links[i]) {
+			if (info.links[i].hasOwnProperty(key)) {
+				output += '<li>' +
+				'<a href= "' + info.links[i][key] +
+				'">' + key + '</a>';
+				'</li>';
 			}
 		}
-
-		var update = document.getElementById('links');
-		update.innerHTML = output;
 	}
+
+	var update = document.getElementById('links');
+	update.innerHTML = output;
 }
-request.send();
